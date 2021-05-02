3b0595f9b2bd799268375d5aac8cfba62ca4e311
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-05-02 16:59:54 +0100

    Constraint Validation API

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Code Starts Here..
+const form = document.querySelector('#form');
+const message = document.querySelector('#message');
+
+let isValid = false;
+
+function validateForm() {
+    isValid = form.checkValidity();
+    console.log(isValid);
+
+    if (!isValid) {
+        message.textContent = 'Please fill out all required fields';
+        message.style.color = 'red';
+    }
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateForm();
+});
